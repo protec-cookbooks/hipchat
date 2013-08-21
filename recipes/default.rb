6e38532008a3032f743cc6bc65ff0b5b26ef4aac
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2013-08-21 10:24:56 +0100

    Fix invalid components line

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 apt_repository "hipchat" do
     uri "http://downloads.hipchat.com/linux/apt"
     distribution "stable"
-    components "main"
+    components ["main"]
     key "https://www.hipchat.com/keys/hipchat-linux.key"
     action :add
 end
